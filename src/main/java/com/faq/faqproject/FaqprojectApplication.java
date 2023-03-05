0b18b51158398c86c4dcd20fa329d46aa5ebe971
pablo <pablomlima75@gmail.com>
2023-03-05 14:02:30 -0300

    Primeiro commit

---
+++ src/main/java/com/faq/faqproject/FaqprojectApplication.java
@@ -0,0 +1,13 @@
+package com.faq.faqproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaqprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaqprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/faq/faqproject/FaqprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faq.faqproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaqprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
